30f9ebc990b91a0e393fedc6526e38d343fc69f9
tobuya <thomasobuya95@gmail.com>
2023-12-21 17:15:12 +0300

    Create and validate user Sign Up

--- core/views.py
+++ core/views.py
@@ -1,6 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from django.http import HttpResponse
+from .models import Profile
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['confirm_password']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already exists')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username already exists')
+                return redirect('signup')
+            else:
+                user = User.objects.create(username=username, email=email, password=password)
+                user.save()
+
+                # Log user in and redirect to profile settings page
+
+                # Create a profile object for the new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('signup')
+        else:
+            messages.info(request, 'Password mismatch')
+            return redirect('signup')
+    else:
+        return render(request, 'signup.html')
